9d2418c393683081fdcc92f14d2aadadb310926b
vitor-sld <48792332+vitor-sld@users.noreply.github.com>
2022-10-27 22:30:30 -0300

    Versão 1.5
    EM PRODUÇÃO: slides animados em js

---
+++ Pizzaria - Site Completo HTML - CSS - JS/JS/script.js
@@ -0,0 +1,8 @@
+var ing = document.querySelector("ul").children[0].innerHTML;
+
+var lista = document.querySelector("ul");
+var novo_item = document.createElement("li");
+
+lista.appendChild(novo_item);
+document.querySelector("ul").children[3].innerHTML="Azeitonas";
+console.log(ing);
